e5d78ca7e67f2d0805ee2e73fe3ef56b788a7c72
hadarlevy <94686791+hadarlevy@users.noreply.github.com>
2022-07-06 21:47:56 +0300

    ver1

--- main.py
+++ main.py
@@ -42,10 +42,25 @@ for i in range(maxi):
             break
         else:
             pass
+    elif f(x1) * f(x2) < 0:
+        test = True
+        print(
+            f"Iteration {i + 1}: \nInitial interval is ({round(x0, 6)},{round(x1, 6)}) \nx{i} = {round(x0, 6)}, f(x{i}) = {round(f(x0), 6)}\tx{i + 1} = {round(x1, 6)}, f(x{i + 1}) = {round(f(x1), 6)}\t x{i + 2} = {round(x2, 6)}, f(x{i + 2}) = {round(f(x2), 6)}")
+        if abs(x2 - x0) < e:
+            print(
+                f"Since |x{i + 2}-x{i}| = {abs(round(x2 - x0, 6))}<{e}, we have reached the stopping condition \n")
+            break
+        else:
+            pass
         x0 = x2
         print(
             f"Since f(x{i+1})*f(x{i+2}) = {round(f(x1)*f(x2),6)}<0, New interval is ({round(x0,6)},{round(x1,6)}) \n")
-
+        if abs(f(x2)) < e:
+            print(
+                f"Since |f({round(x2, 6)})| = {abs(round(f(x2), 6))}<{e}, we have reached the stopping condition \n")
+            break
+        else:
+            pass
 else:
     test = False
     print("No root in the interval")
